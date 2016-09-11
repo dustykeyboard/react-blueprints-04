f1c1fa14d5243a5292e0d06b9a9ac2d93ebc9c5e
Philip Boardman <dustykeyboard@brd.mn>
2016-09-11 21:57:14 +1000

    Adding regex search

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ var searchDb = mongoose.model('sites', siteSchema);
 var routes = function (app) {
   app.use(bodyparser.json());
   app.get('/search/:title', function (req, res) {
-    searchDb.find({ title: req.params.title }, function (err, data) {
-      if (err) return res.status(500)
-        .send({
-          'msg': 'couldn\'t find anything'
-        });
-      res.json(data);
-    });
+    searchDb.find({
+      title:
+      { $regex: '^' + req.params.title + '*', $options: 'i' }
+    },
+      function (err, data) {
+        res.json(data);
+      });
   });
 };
 
@@ -34,6 +34,5 @@ routes(router);
 app.use('/v1', router);
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
-  console.log('server listening on port ' + (process.env.PORT ||
-    port));
+  console.log('listening on :' + (process.env.PORT || port));
 });
